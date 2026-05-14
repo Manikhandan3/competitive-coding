5be378dea8d313e4f9ef7a680f8ca401d68eeb14
Manikhandan Senthilvelu <mavureddysenthilve.m@northeastern.edu>
2026-05-14 14:14:17 -0400

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        half = total // 2
+        cache = { 0 : True}
+
+        def dfs(i,s):
+            if s in cache:
+                return cache[s]
+            
+            if s < 0:
+                return False
+            
+            if i == len(nums):
+                return False
+            
+            if dfs(i+1,s-nums[i]):
+                cache[s - nums[i]] = True
+                return True
+            
+            if dfs(i+1,s):
+                cache[s] = True
+                return True
+            
+            cache[s] = False
+            return False
+        
+        return dfs(0,half)
