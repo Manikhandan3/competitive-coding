fe17cc1cc1184f3ebea2dee0b5ce642a3e69b0cb
Manikhandan Senthilvelu <mavureddysenthilve.m@northeastern.edu>
2026-09-24 09:23:07 -0400

    Add: count-connected-components - submission-11

---
+++ Data Structures & Algorithms/count-connected-components/submission-11.py
@@ -0,0 +1,34 @@
+class DSU:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+        self.components = n
+    
+    def find(self,node):
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    
+    def union(self,u,v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+        self.components -= 1
+        if self.rank[pu] < self.rank[pv]:
+            pu, pv = pv, pu
+        self.parent[pv] = pu
+        self.rank[pu] += self.rank[pv]
+        return True
+    
+    def comps(self):
+        return self.components
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = n
+        for u,v in edges:
+            if dsu.union(u,v):
+                res -= 1
+        return res
